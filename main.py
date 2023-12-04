14855a2a2204f682976e00867cc220e0d7d167b5
Rob <rlmartain@hotmail.com>
2023-12-05 07:19:44 +1100

    Update main.py

--- main.py
+++ main.py
@@ -36,76 +36,177 @@ class ServoControl:
 		print("Servo Test Requested")
 
 class DriveMotorControl:
-	driveLeftForwardPin = 27
-	driveLeftBackwardsPin = 22
-	driveLeftEnablePin = 13
-	driveLeftPWM = None
-
-	driveRightForwardPin = 23
-	driveRightBackwardsPin = 24
-	driveRightEnablePin = 18
+	driveRightForwardPin = 27
+	driveRightBackwardPin = 22
+	driveRightEnablePin = 13
 	driveRightPWM = None
+
+	driveLeftForwardPin = 23
+	driveLeftBackwardPin = 24
+	driveLeftEnablePin = 18
+	driveLeftPWM = None
 
 	pwmFreq = 1000
 
 	def __init__(self):
 		print("Drive constructor called")
 
+		GPIO.setup(self.driveLeftForwardPin, GPIO.OUT)
+		GPIO.setup(self.driveLeftBackwardPin, GPIO.OUT)
+		GPIO.setup(self.driveLeftEnablePin, GPIO.OUT)
+		GPIO.setup(self.driveRightForwardPin, GPIO.OUT)
+		GPIO.setup(self.driveRightBackwardPin, GPIO.OUT)
+		GPIO.setup(self.driveRightEnablePin, GPIO.OUT)
+
+		GPIO.output(self.driveLeftForwardPin, GPIO.LOW)
+		GPIO.output(self.driveLeftBackwardPin, GPIO.LOW)
+		GPIO.output(self.driveRightForwardPin, GPIO.LOW)
+		GPIO.output(self.driveRightBackwardPin, GPIO.LOW)
+
+		self.driveLeftPWM = GPIO.PWM(self.driveLeftEnablePin, self.pwmFreq)
+		self.driveLeftPWM.start(0)
+		self.driveRightPWM = GPIO.PWM(self.driveRightEnablePin, self.pwmFreq)
+		self.driveRightPWM.start(0)
+
 	def AllStop(self):
 		print("AllStop Called")
+		self.driveLeftPWM.start(0)
+		self.driveRightPWM.start(0)
+
+		GPIO.output(self.driveLeftForwardPin, GPIO.LOW)
+		GPIO.output(self.driveLeftBackwardPin, GPIO.LOW)
+		GPIO.output(self.driveRightForwardPin, GPIO.LOW)
+		GPIO.output(self.driveRightBackwardPin, GPIO.LOW)
 
 	def SetPower(self, leftDirection, rightDirection, leftPower, rightPower, ramp=False):
-		print("Set Power Called")
+		GPIO.output(self.driveLeftForwardPin, GPIO.LOW)
+		GPIO.output(self.driveLeftBackwardPin, GPIO.LOW)
+		GPIO.output(self.driveRightForwardPin, GPIO.LOW)
+		GPIO.output(self.driveRightBackwardPin, GPIO.LOW)
+
+		if (leftDirection == MotorDirection.FORWARD):
+			GPIO.output(self.driveLeftForwardPin, GPIO.HIGH)
+		else:
+			GPIO.output(self.driveLeftBackwardPin, GPIO.HIGH)
+
+		if (rightDirection == MotorDirection.FORWARD):
+			GPIO.output(self.driveRightForwardPin, GPIO.HIGH)
+		else:
+			GPIO.output(self.driveRightBackwardPin, GPIO.HIGH)
+
+		if (ramp):
+			startingPower = 25
+			rampBy = startingPower
+			rampLeftPower = startingPower
+			rampRightPower = startingPower
+			while (rampBy < 100):
+				rampBy = rampBy + 1
+				if (rampLeftPower < leftPower):
+					rampLeftPower = rampLeftPower + 1
+				if (rampRightPower < rightPower):
+					rampRightPower = rampRightPower + 1
+
+				self.driveLeftPWM.start(rampLeftPower)
+				self.driveRightPWM.start(rampRightPower)
+				sleep(0.1)
+		else:
+			self.driveLeftPWM.start(leftPower)
+			self.driveRightPWM.start(rightPower)
 
 	def Test(self):
 		print("Drive Test Requested")
-
+		driveMotorControl.SetPower(MotorDirection.FORWARD, MotorDirection.FORWARD, 50, 50)
+		sleep(0.5)
+		driveMotorControl.SetPower(MotorDirection.FORWARD, MotorDirection.FORWARD, 100, 100)
+		sleep(0.5)
+		driveMotorControl.AllStop()
+		sleep(1)
+		driveMotorControl.SetPower(MotorDirection.BACKWARD, MotorDirection.BACKWARD, 50, 50)
+		sleep(1)
+		driveMotorControl.SetPower(MotorDirection.BACKWARD, MotorDirection.BACKWARD, 100, 100)
+		sleep(1)
+		driveMotorControl.AllStop()
+		sleep (1)
+		driveMotorControl.SetPower(MotorDirection.FORWARD, MotorDirection.FORWARD, 100, 100, True)
+		driveMotorControl.AllStop()
 
 #==============
 # SSH Keyboard Input
 #==============
 lastKeyPressed="s"
+pressNumber = 0
 
 def keyPressed(key):
-	global lastKeyPressed
+	global lastKeyPressed, pressNumber
 
-	firstPress = False
+	pressPower = 0
 
-	print("Key Pressed", key)
+	print(key, lastKeyPressed)
 
 	if(key == lastKeyPressed):
-		firstPress = False
+		pressNumber = pressNumber + 1
+		print("additional")
 	else:
-		firstPress = True
-
-	if (key == ""):
-		print("")
+		pressNumber = 1
 
 	lastKeyPressed = key
 
+	if (pressNumber == 1):
+		pressPower = 50
+	if (pressNumber == 2):
+		pressPower = 75
+	if (pressNumber >= 3):
+		pressPower = 100
+
+	print(key, pressNumber, pressPower)
+
+	if (key == "t"):
+		driveMotorControl.Test()
+	if (key == "w"):
+		driveMotorControl.SetPower(MotorDirection.FORWARD, MotorDirection.FORWARD, pressPower, pressPower)
+	if (key == "x"):
+		driveMotorControl.SetPower(MotorDirection.BACKWARD, MotorDirection.BACKWARD, pressPower, pressPower)
+	if (key == "a"):
+		driveMotorControl.SetPower(MotorDirection.BACKWARD, MotorDirection.FORWARD, pressPower, pressPower)
+	if (key == "d"):
+		driveMotorControl.SetPower(MotorDirection.FORWARD, MotorDirection.BACKWARD, pressPower, pressPower)
+	if (key == "s"):
+		driveMotorControl.AllStop()
+		pressNumber = 0
+
+
 #==============
-# Execution Ppoint
+# Execution Point
 #=============
 
 ready = False
 driveMotorControl = None
 servoControl = None
-
+print("Execution point commencing")
 
 GPIO.setmode(GPIO.BCM)
+
+startupErrorCount = 0
+
 while (True):
+	if (startupErrorCount > 3):
+		print("Main execution failed.  Exiting.")
+		break
 	if (not ready):
-		try:
+		print("Reports not ready.  Initialising")
+		if True: #try:
 			servoControl = ServoControl()
 			driveMotorControl = DriveMotorControl()
 
 			ready = True
+			print("Successful startup, setting ready = true")
 
 			if (doTests):
 				servoControl.Test()
 				driveMotorControl.Test()
 
 			listen_keyboard(on_press=keyPressed)
-
-		except:
+			print("Initialization completed")
+		#except:
+			startupErrorCount = startupErrorCount + 1
 			print("Main Execution Startup Error")
